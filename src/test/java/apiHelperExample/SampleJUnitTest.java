ea664044870d0c309778372f19d5b6a613d652a3
Anna Chernyshova <anna_chernyshova@epam.com>
2020-10-01 14:14:33 +0300

    added second test

--- src/test/java/apiHelperExample/SampleJUnitTest.java
+++ src/test/java/apiHelperExample/SampleJUnitTest.java
@@ -3,6 +3,8 @@ package apiHelperExample;
 import apiHelperExample.pages.HomePage;
 import io.restassured.http.ContentType;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.parallel.Execution;
+import org.junit.jupiter.api.parallel.ExecutionMode;
 
 import java.util.LinkedList;
 import java.util.List;
@@ -11,8 +13,10 @@ import java.util.stream.Collectors;
 import static io.restassured.RestAssured.given;
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.Matchers.arrayContainingInAnyOrder;
+import static org.hamcrest.Matchers.equalTo;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
+@Execution(ExecutionMode.CONCURRENT)
 public class SampleJUnitTest extends JUnitTestBase {
 
     private HomePage homepage;
@@ -26,9 +30,19 @@ public class SampleJUnitTest extends JUnitTestBase {
         driver.get(baseUrl);
         List<String> actualRepositoriesList = homepage.searchForRepositories(searchQuery)
                 .getRepoListNamesFromPage();
-        assertTrue(actualRepositoriesList.stream().allMatch(item -> item.contains(searchQuery)),
-                String.format("List Elements: [%s] does not contains text [%s] ", actualRepositoriesList, searchQuery));
         List<String> expectedRepositoriesList = repositoriesApiHelper.getRepoNamesForSearchQuery(searchQuery);
         assertThat(actualRepositoriesList.toArray(), arrayContainingInAnyOrder(expectedRepositoriesList.toArray()));
+
+    }
+
+    @Test
+    public void testRepositoriesListSearch1() {
+        repositoriesApiHelper = new RepositoriesApiHelper();
+        String searchQuery = "healenium";
+        homepage = new HomePage(driver);
+        driver.get(baseUrl);
+        List<String> actualRepositoriesList = homepage.searchForRepositories(searchQuery)
+                .getRepoListNamesFromPage();
+        assertThat(actualRepositoriesList.size(), equalTo(9));
     }
 }
