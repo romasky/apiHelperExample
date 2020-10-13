3d98a365a947018e7e420943d5e7865b7bcb3f09
Anna Chernyshova <anna_chernyshova@epam.com>
2020-10-13 20:55:55 +0300

    added screenshots

--- src/test/java/apiHelperExample/pages/HomePage.java
+++ src/test/java/apiHelperExample/pages/HomePage.java
@@ -1,12 +1,15 @@
 package apiHelperExample.pages;
 
 
+import io.qameta.allure.Allure;
 import io.qameta.allure.Step;
-import org.openqa.selenium.Keys;
-import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.WebElement;
+import org.openqa.selenium.*;
 import org.openqa.selenium.support.CacheLookup;
 import org.openqa.selenium.support.FindBy;
+
+import java.io.ByteArrayInputStream;
+
+import static java.lang.String.format;
 
 
 /**
@@ -14,23 +17,25 @@ import org.openqa.selenium.support.FindBy;
  */
 public class HomePage extends Page {
 
-  @FindBy(css = ".header-search-input")
-  @CacheLookup
-  public WebElement searchField;
+    @FindBy(css = ".header-search-input")
+    @CacheLookup
+    public WebElement searchField;
 
-  public HomePage(WebDriver webDriver) {
-    super(webDriver);
-  }
+    public HomePage(WebDriver webDriver) {
+        super(webDriver);
+    }
 
-  @Step("Открыта страница {baseUrl}")
-  public void open(String baseUrl) {
-    driver.get(baseUrl);
-  }
+    @Step("Открыта страница {baseUrl}")
+    public void open(String baseUrl) {
+        driver.get(baseUrl);
+        Allure.addAttachment("Главная страница GitHub", new ByteArrayInputStream(((TakesScreenshot) driver).getScreenshotAs(OutputType.BYTES)));
+    }
 
-  @Step("Ищем репозитории по ключевому слову {value}")
-  public RepositoriesPage searchForRepositories(String value) {
-    waitForElement(searchField).sendKeys(value);
-    waitForElement(searchField).sendKeys(Keys.ENTER);
-    return new RepositoriesPage(driver);
-  }
+    @Step("Ищем репозитории по ключевому слову {value}")
+    public RepositoriesPage searchForRepositories(String value) {
+        waitForElement(searchField).sendKeys(value);
+        waitForElement(searchField).sendKeys(Keys.ENTER);
+        Allure.addAttachment(format("Найденные репозитории по ключевому слову ", value), new ByteArrayInputStream(((TakesScreenshot) driver).getScreenshotAs(OutputType.BYTES)));
+        return new RepositoriesPage(driver);
+    }
 }
